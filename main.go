d7df3dfdbb88f59a38a9e8e1d5d644a2c067e2f4
Kurlgargyey <nahsirkivar@gmail.com>
2025-01-14 10:00:38 +0100

    set readheadertimeout on server correctly

--- main.go
+++ main.go
@@ -91,8 +91,8 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 5,
 	}
-	srv.ReadHeaderTimeout = 5
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
